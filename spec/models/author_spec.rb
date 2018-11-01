0dd36db20550d3484f5fe27ae6b4f4d03a7dc797
Maximilian <koenigmax1@web.de>
2018-11-01 16:32:17 +0100

    Closes #3

--- spec/models/author_spec.rb
+++ spec/models/author_spec.rb
@@ -1,11 +1,13 @@
-require 'rails helper'
+require 'rails_helper'
 
-describe Author, type :model do
+describe Author, type: :model do
 
-    author = Author.new('Alan', 'Turing', 'http://wikipedia.org/Alan_Turing')
-    expect(author.first_name).to eq('Alan')
-    expect(author.last_name).to eq('Turing')
-    expect(author.homepage).to eq('http://wikipedia.org/Alan_Turing')
-    expect(autor.name()).to eq('Alan Turing')
+    it "should create a new author object correctly" do
+        author = Author.new(first_name: "Alan", last_name: "Turing", homepage: "http://wikipedia.org/Alan_Turing")
+        expect(author.first_name).to eq("Alan")
+        expect(author.last_name).to eq("Turing")
+        expect(author.homepage).to eq("http://wikipedia.org/Alan_Turing")
+        expect(author.name()).to eq("Alan Turing")
+    end
 
 end
