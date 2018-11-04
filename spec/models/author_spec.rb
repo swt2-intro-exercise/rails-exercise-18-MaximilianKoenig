29652d36387332f37b74524503eda12ba073b505
Maximilian <koenigmax1@web.de>
2018-11-04 17:09:28 +0100

    Failing test for #7

--- spec/models/author_spec.rb
+++ spec/models/author_spec.rb
@@ -10,4 +10,9 @@ describe Author, type: :model do
         expect(author.name()).to eq("Alan Turing")
     end
 
+    it "should check whether an author has a last name" do
+      author = Author.new(first_name: "Alan", last_name: "", homepage: "http://wikipedia.org/Alan_Turing")
+      expect(author).to_not be_valid
+    end
+
 end
