e97f9b3f3b12283b904f2d13af203825127f5d7d
Maximilian <koenigmax1@web.de>
2018-11-04 17:34:01 +0100

    Failing test for #8

--- spec/features/author/new_author_spec.rb
+++ spec/features/author/new_author_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 describe "New author page", type: :feature do
   
-  it "should render withour error" do
+  it "should render without error" do
     visit new_author_path
   end
 
@@ -21,6 +21,15 @@ describe "New author page", type: :feature do
     page.fill_in 'author[last_name]', with: 'Turing'
     page.fill_in 'author[homepage]', with: 'http://wikipedia.org/Alan_Turing'
     find('input[type="submit"]').click
+  end
+
+  it "should show an error page containing what went wrong when not filling out the last name" do
+    visit new_author_path
+
+    page.fill_in 'author[first_name]', with: 'Alan'
+    page.fill_in 'author[homepage]', with: 'http://wikipedia.org/Alan_Turing'
+    find('input[type="submit"]').click
+    expect(page).to have_text("Last_name can't be blank")
   end
 
 end
