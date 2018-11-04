386ed8d8762a8d98186cb18b0fc053509520729c
Maximilian <koenigmax1@web.de>
2018-11-04 17:59:14 +0100

    Failint test for #9

--- spec/features/author/authors_index_page_spec.rb
+++ spec/features/author/authors_index_page_spec.rb
@@ -2,14 +2,17 @@ require 'rails_helper'
 
 describe "Authors index page", type: :feature do
 
+    before(:each) do
+      @author = FactoryBot.create :author
+      visit authors_path
+    end
+
     it "should have a link to add new authors and list all authors with their names and homepages and the link to their detail page" do
-        author = FactoryBot.create :author
-        visit authors_path
 
         expect(page).to have_link 'New Author', href: new_author_path
-        expect(page).to have_text("#{author.name}")
-        expect(page).to have_text("#{author.homepage}")
-        expect(page).to have_link 'Details', href: author_path(author)
+        expect(page).to have_text("#{@author.name}")
+        expect(page).to have_text("#{@author.homepage}")
+        expect(page).to have_link 'Details', href: author_path(@author)
     end
     
 end
