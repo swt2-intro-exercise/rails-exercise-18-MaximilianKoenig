f07b655271d20219d7b0dac14105d44095e3a24c
Maximilian <koenigmax1@web.de>
2018-11-06 15:27:54 +0100

    Failing test for #12

--- spec/features/paper/new_paper_spec.rb
+++ spec/features/paper/new_paper_spec.rb
@@ -5,5 +5,18 @@ describe "New paper page", type: :feature do
   it "should render without error" do
     visit new_paper_path
   end
-  
+
+  it "should show an error page containing what went wrong when any attribute is empty or year is not numeric" do
+    visit new_paper_path
+
+    page.fill_in 'paper[title]', with: 'Stuff'
+    page.fill_in 'paper[venue]', with: 'Book'
+    page.fill_in 'paper[year]', with: 'Eleven'
+    find('input[type="submit"]').click
+    expect(page).to have_text("Title can't be blank")
+    expect(page).to have_text("Venue can't be blank")
+    expect(page).to have_text("Year can't be blank")
+    expect(page).to have_text("Year is not a number")
+  end
+
 end
