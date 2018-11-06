53e2ef9979a39a7568710768162e07072deedfc9
Maximilian <koenigmax1@web.de>
2018-11-06 15:30:01 +0100

    Quickfix

--- spec/features/paper/new_paper_spec.rb
+++ spec/features/paper/new_paper_spec.rb
@@ -9,9 +9,9 @@ describe "New paper page", type: :feature do
   it "should show an error page containing what went wrong when any attribute is empty or year is not numeric" do
     visit new_paper_path
 
-    page.fill_in 'paper[title]', with: 'Stuff'
-    page.fill_in 'paper[venue]', with: 'Book'
-    page.fill_in 'paper[year]', with: 'Eleven'
+    page.fill_in 'paper[title]', with: ''
+    page.fill_in 'paper[venue]', with: ''
+    page.fill_in 'paper[year]', with: ''
     find('input[type="submit"]').click
     expect(page).to have_text("Title can't be blank")
     expect(page).to have_text("Venue can't be blank")
