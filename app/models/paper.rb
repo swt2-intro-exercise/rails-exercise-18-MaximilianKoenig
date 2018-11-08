5f4b893b77b21a60f16080fc1c8d7fcca8d6cb53
Maximilian <koenigmax1@web.de>
2018-11-08 17:50:12 +0100

    Closes #16 Closes #17

--- app/models/paper.rb
+++ app/models/paper.rb
@@ -2,6 +2,8 @@ class Paper < ApplicationRecord
 
   has_and_belongs_to_many :authors
 
+  scope :published_at, ->(year) { where("year = ?", year)}
+
   validates :title, :venue, :year, presence: true
   validates :year, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 end
